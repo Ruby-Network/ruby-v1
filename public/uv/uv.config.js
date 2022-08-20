535cdd27d2688338d5d1f3b56caaaeeab63c93db
XxHiroTTVxX <101534563+XxHiroTTVxX@users.noreply.github.com>
2022-08-20 15:17:42 -0700

    Bare change

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/',
-    bare: 'https://weeb-central.icu/bare/',
+      bare: location.origin + '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
