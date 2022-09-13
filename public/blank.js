a4f3288ff71ed6b6e17c465dfd06705df484dc7b
XxHiroTTVxX <101534563+XxHiroTTVxX@users.noreply.github.com>
2022-09-13 00:41:25 -0700

    Update blank.js

--- public/blank.js
+++ public/blank.js
@@ -34,7 +34,7 @@ open() {
 if (this.type == "blank") {
 try {
 var page = window.open()
-page.document.body.innerHTML = `<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="https://anonymouss.org"></iframe>`
+page.document.body.innerHTML = `<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="https://weeb-central.icu"></iframe>`
 console.log(this.url);
 } catch {
 }
